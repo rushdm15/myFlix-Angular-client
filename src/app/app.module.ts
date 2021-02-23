eb9292878a1aaeb9e3bfb566ad50c45b79ae9598
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-02-23 17:48:30 +0200

    MatIconModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -39,6 +40,7 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
     MatInputModule,
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
     MatFormFieldModule,
     MatSnackBarModule,
     MatGridListModule,
