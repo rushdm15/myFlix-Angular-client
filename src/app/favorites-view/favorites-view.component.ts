5c68fd3fa9206039502be372634a9bf89ef4954a
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-02-24 18:25:26 +0200

    favorites-view, components, dialogs, routing

--- src/app/favorites-view/favorites-view.component.ts
+++ src/app/favorites-view/favorites-view.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { GetFavoriteMoviesService } from '../fetch-api-data.service';
+import { GenreViewComponent } from '../genre-view/genre-view.component';
+import { DirectorViewComponent } from '../director-view/director-view.component';
+import { SynopsisViewComponent } from '../synopsis-view/synopsis-view.component';
+// import { FavoritesViewComponent } from '../favorites-view/favorites-view.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-favorites-view',
@@ -6,10 +12,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorites-view.component.scss']
 })
 export class FavoritesViewComponent implements OnInit {
+  movies: any[] = [];
+  loading = true;
+  constructor(
+    public fetchApiData: GetFavoriteMoviesService,
+    public dialog: MatDialog
+    ) { }
 
-  constructor() { }
+ngOnInit(): void {
+  this.getFavorites();
+}
 
-  ngOnInit(): void {
+getFavorites(): void {
+  this.fetchApiData.getFavoriteMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      this.loading = false;
+      console.log(this.movies);
+      return this.movies;
+    });
   }
-
+  openGenreViewDialog(): void {
+    this.dialog.open(GenreViewComponent, {
+      width: '280px'
+    });
+  }
+  openDirectorViewDialog(): void {
+    this.dialog.open(DirectorViewComponent, {
+      width: '280px'
+    });
+  }
+  openSynopsisViewDialog(): void {
+    this.dialog.open(SynopsisViewComponent, {
+      width: '280px'
+    });
+  }
+  // openFavoritesViewDialog(): void {
+  //   this.dialog.open(FavoritesViewComponent, {
+  //     width: '280px'
+  //   });
+  // }
 }
