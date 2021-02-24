b34d9c0751eefc27e3d7b67aba9bdd921e7233b4
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-02-24 18:17:59 +0200

    profile vieww components, dialogs, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {WelcomePageComponent} from './welcome-page/welcome-page.component';
 import {MovieCardComponent} from './movie-card/movie-card.component';
 
+import {ProfileViewComponent} from './profile-view/profile-view.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: ProfileViewComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 
--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
+import { FavoritesViewComponent } from '../favorites-view/favorites-view.component';
+// import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 
 @Component({
   selector: 'app-profile-view',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-view.component.scss']
 })
 export class ProfileViewComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(public dialog: MatDialog) { }
   ngOnInit(): void {
   }
-
+  // openUserRegistrationDialog(): void {
+  //   this.dialog.open(UserRegistrationFormComponent, {
+  //     width: '280px'
+  //   });
+  // }
+  openFavoritesViewDialog(): void {
+    this.dialog.open(FavoritesViewComponent, {
+      width: '280px'
+    });
+  }
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    });
+  }
+  openProfileViewDialog(): void {
+    this.dialog.open(ProfileViewComponent, {
+      width: '500px'
+    });
+  }
 }
